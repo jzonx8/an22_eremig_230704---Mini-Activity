df11802bb3362bcc1e7f48308a90247a26768e59
Emmanuel Jazon <131515197+jzonx8@users.noreply.github.com>
2023-07-06 09:33:23 +0800

    Update courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const courseController = require('../controllers/courseControllers')
 
-router.post('/checkEmail', (req, res) => {
+router.post('/MainActivity', (req, res) => {
 	courseController.checkEmailExists(req.body)
 	.then(resultFromController => res.send(resultFromController));
 })
